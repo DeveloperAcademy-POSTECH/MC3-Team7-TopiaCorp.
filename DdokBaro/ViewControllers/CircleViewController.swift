a767bd17eb99312984638273c1f58366c624d150
seoyeon-1206 <ssy_1206@naver.com>
2023-07-18 11:16:44 +0900

    ✨ feat: 추가 WelcomeView, CircleView (#1)

---
+++ DdokBaro/ViewControllers/CircleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CircleView.swift
+//  DdokBaro
+//
+//  Created by 신서연 on 2023/07/16.
+//
+
+import UIKit
+
+class CircleViewController : UIView{
+
+    override func draw(_ rect: CGRect) {
+            let ellipseRect = CGRect(x:0, y:0, width: 530, height: 177)
+            
+            let path = UIBezierPath(ovalIn: bounds)
+            
+            let shadowLayer = CALayer()
+            shadowLayer.frame = CGRect(x: bounds.origin.x, y: bounds.origin.y, width: bounds.width, height: bounds.height / 2)
+            shadowLayer.shadowColor = UIColor.systemGray.cgColor
+            shadowLayer.shadowOpacity = 0.5
+            shadowLayer.shadowOffset = CGSize(width: 4, height: 4)
+            shadowLayer.shadowRadius = 13
+            shadowLayer.masksToBounds = false
+            
+            layer.addSublayer(shadowLayer)
+        
+        let maskLayer = CAShapeLayer()
+                maskLayer.path = path.cgPath
+                layer.mask = maskLayer
+                
+                UIColor.yellow.setFill()
+                path.fill()
+    }
+}
+
