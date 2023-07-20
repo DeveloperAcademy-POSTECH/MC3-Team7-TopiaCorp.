f9df51f995ea3c6c5598e062a31301a7b91aa5bb
LIM-YUSANG <119108929+LIM-YUSANG@users.noreply.github.com>
2023-07-20 09:21:09 +0900

    ✨ feat: 추가 각도따라 이미지 움직이기 (#17)

--- DdokBaro/ViewControllers/MotionViewController.swift
+++ DdokBaro/ViewControllers/MotionViewController.swift
@@ -13,18 +13,43 @@ import UserNotifications
 class MotionViewController: UIViewController, CMHeadphoneMotionManagerDelegate {
     
     @IBOutlet weak var textView: UITextView!
-    //@IBOutlet weak var textView: UITextView!
+    
     @IBOutlet weak var textView2: UITextView!
     
-    @IBOutlet weak var Image: UIImageView!
+    @IBOutlet weak var turtleimage: UIImageView!
+    
+    var intPitch: Int = 0
+    
     //AirPods Pro => manager :) 헤드폰 모니터 매니저 담는 상수
     let manager = CMHeadphoneMotionManager()
+    
     private var timer = Timer()
+    
+    enum angle: Int {
+        case notgood = 20
+        case bad = 30
+        case danger = 40
+    }
+    
     override func viewDidLoad() {
         NotificationManager().requestAuthorization()
         super.viewDidLoad()
-        //var timerCounting:Bool = false
         textView.text = "에어팟 프로를 연결해주세요"
+        
+        let animation = LottieAnimation.named("TurtleBody")
+        let animationView = LottieAnimationView(animation: animation)
+        
+        let animation2 = LottieAnimation.named("TurtleHead")
+        let animationView2 = LottieAnimationView(animation: animation2)
+        
+        let animation3 = LottieAnimation.named("WaterDrops1")
+        let animationView3 = LottieAnimationView(animation: animation3)
+        
+        let animation4 = LottieAnimation.named("WaterDrops2")
+        let animationView4 = LottieAnimationView(animation: animation4)
+        
+        let animation5 = LottieAnimation.named("WaterDrops3")
+        let animationView5 = LottieAnimationView(animation: animation5)
         
         //헤드폰 모니터링 할려면 delegate 대리자 필요
         manager.delegate = self
@@ -47,7 +72,7 @@ class MotionViewController: UIViewController, CMHeadphoneMotionManagerDelegate {
                     let y = motion.attitude.quaternion.y
                     let z = motion.attitude.quaternion.z
                     let w = motion.attitude.quaternion.w
-                    let intpitch = degreeInt(pitch)
+                    intPitch = degreeInt(pitch)
                     
                     //디스패치큐 사용해서 cpu효율적으로 사용, 디스패치큐는 딱히 사용할 필요는 없음
                     DispatchQueue.main.async { [self] in
@@ -59,53 +84,84 @@ class MotionViewController: UIViewController, CMHeadphoneMotionManagerDelegate {
                         str += "\nQuaternion:\n"
                         str += quaternion(x, y, z, w)
                         textView.text = str
+                        
+                        self.view.addSubview(animationView2)
+                        animationView2.frame = self.view.bounds
+                        animationView2.center = self.view.center
+                        animationView2.contentMode = .scaleAspectFit
+                        animationView2.animationSpeed = 1.0
+                        //animationView2.currentProgress = AnimationProgressTime(pitch/80)
+                        if pitch > 0 {
+                            animationView2.currentProgress = AnimationProgressTime(0)
+                        }
+                        else{
+                            animationView2.currentProgress = AnimationProgressTime(-pitch/40)
+                        }
+                        
+                        self.view.addSubview(animationView)
+                        
                     }
                     
-                    if intpitch > 40 {
-                        print("피치 40 이상")
+                    
+                    //만약 목 각도가 정해진 기준 이상이면
+                    if intPitch < angle.notgood.rawValue {
+                        self.view.addSubview(animationView3)
+                        animationView3.translatesAutoresizingMaskIntoConstraints = false
+                        animationView3.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 280).isActive = true
+                        //animationView3.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
+                        animationView3.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -190).isActive = true
+
+                        animationView3.play()
+                        print("피치 35 이상")
+                        
+                        //타이머가 없을 경우 타이머 작동, 타이머가 있을경우에는 다시 타이머가 작동되지 않도록
                         if timer.isValid {
                             
                         }
                         else {
-                            timer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(turtleAlert), userInfo: nil, repeats: false)
+                            timer = Timer.scheduledTimer(timeInterval: 180, target: self, selector: #selector(turtleAlert), userInfo: nil, repeats: false)
                             print("타이머 실행")
                             //timerCounting = true
                         }
                     }
+                    //만약 목 각도가 기준선 이하로 돌아오면 타이머 삭제
                     else{
                         timer.invalidate()
                     }
                     
-                    //                let animation = LottieAnimation.named("sample")
-                    //                let animationView = LottieAnimationView(animation: animation)
-                    //                self.view.addSubview(animationView)
-                    //                animationView.frame = self.view.bounds
-                    //                animationView.center = self.view.center
-                    //                animationView.contentMode = .scaleAspectFit
-                    //                animationView.animationSpeed = 1.0
-                    //                animationView.play(fromFrame: AnimationFrameTime(intpitch)*2.7, toFrame: AnimationFrameTime(intpitch)*2.8)
-                    
                 } else {
                     textView.text = "ERROR: \(error?.localizedDescription ?? "")"
                 }
+                
             })
         
         
+        //거북이 몸통을 위에 겹치기 위해 여기에 작성
+        animationView.frame = self.view.bounds
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFit
+        animationView.animationSpeed = 1.0
+        animationView.loopMode = .loop
+        animationView.play()
+
     }
+    
     //degree 텍스트 출력위해
     func degreeText(_ label: String, _ num: Double) -> String {
-        return String(format: "\(label): %.0fº\n", abs(num))
+        return String(format: "\(label): %.0fº\n", num)
     }
+    
     //degree int로 바꿈
     func degreeInt(_ num: Double) -> Int {
-        return Int(abs(num))
+        return Int(num)
     }
     
     //에어팟 연결되었을때
     func headphoneMotionManagerDidConnect(_ manager: CMHeadphoneMotionManager) {
-        textView.text = "에어팟 연결됨"
+        textView.text = "에어팟 연결 성공"
         print("에어팟 연결 성공")
     }
+    
     //에어팟 연결 끊겼을때
     func headphoneMotionManagerDidDisconnect(_ manager: CMHeadphoneMotionManager) {
         textView.text = "에어팟 연결 끊김"
@@ -139,7 +195,7 @@ class MotionViewController: UIViewController, CMHeadphoneMotionManagerDelegate {
     
     //설정한 시간동안 거북목일 경우 알람 울리게 한다
     @objc func turtleAlert() {
-        print("타이머 실험")
+        //print("타이머 실험")
         NotificationManager().scheduleNotification()
     }
 }
