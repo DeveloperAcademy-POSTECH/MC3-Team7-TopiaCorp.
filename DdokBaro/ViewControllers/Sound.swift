28adf254eb14db38b46f6631d354d05fedf7577b
LIM-YUSANG <119108929+LIM-YUSANG@users.noreply.github.com>
2023-07-25 00:07:38 +0900

    🐛 bug 수정 Haptics, Sound(#31)

--- DdokBaro/ViewControllers/Sound.swift
+++ DdokBaro/ViewControllers/Sound.swift
@@ -10,63 +10,37 @@ import AVFoundation
 
 private var player: AVAudioPlayer?
 
-enum Sound: String {
-    case drop = "Drop"
-    case rain = "Rain"
-}
-
-func playSoundDrop() {
-    let soundName = Sound.drop.rawValue
-    
+func playSound(soundName: String, rate: Float) {
     guard let url = Bundle.main.url(forResource: soundName, withExtension: "mp3") else {
+        print("Error: Sound file not found.")
         return
     }
     
     do {
-        try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
+        try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode:  AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.duckOthers])
         try AVAudioSession.sharedInstance().setActive(true)
     } catch {
-        // print error...
+        
     }
     
     do {
         player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-        player?.numberOfLoops = -1
+        player?.enableRate = true
+        player?.rate = rate
         player?.play()
-        //try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode:  AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
     } catch let error {
         print(error.localizedDescription)
     }
-}
 
-func playSoundRain() {
-    let soundName2 = Sound.rain.rawValue
-    
-    guard let url = Bundle.main.url(forResource: soundName2, withExtension: "mp3") else {
-        return
-    }
-    
-    do {
-        //try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-        try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode:  AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
-        try AVAudioSession.sharedInstance().setActive(true)
-    } catch {
-        // print error...
-    }
-    
-    do {
-        player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-        //player?.numberOfLoops = -1
-        player?.play()
-        print("플레이 되는중")
-//        try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode:  AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
-    } catch let error {
-        print(error.localizedDescription)
-    }
 }
 
 func stopSound() {
     player?.stop()
+    do {
+        try AVAudioSession.sharedInstance().setActive(false)
+    } catch {
+        
+    }
 }
 
 
