7ce6c4e086bee84acfc502acb138b7db4970c026
geee3 <c1as2icblu3@gmail.com>
2023-07-30 19:17:01 +0900

    ✨ feat: 구현 grassGraph level (#52)

--- DdokBaro/Views/GrassGraphView.swift
+++ DdokBaro/Views/GrassGraphView.swift
@@ -8,21 +8,23 @@
 import UIKit
 
 class GrassGraphView: UIView {
+    var level: Int
+    var colors = ["EBEDF0", "9BE9A8", "40C463", "30A14E", "216E39"]
     
-    override init(frame: CGRect) {
+    required init(frame: CGRect, level: Int) {
+        self.level = level
         super.init(frame: frame)
         backgroundColor = .clear
     }
     
     required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        backgroundColor = .clear
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func draw(_ rect: CGRect) {
         let path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: rect.width, height: rect.height),
                                 byRoundingCorners:.allCorners, cornerRadii: CGSize(width: rect.width / 6, height: rect.width / 6))
-        UIColor.lightGray.setFill()
+        UIColor(hexCode: colors[level]).setFill()
         path.fill()
         path.close()
     }
